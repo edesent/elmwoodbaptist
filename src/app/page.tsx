03e369a60ebc6d266c0979a1601b96058e8d675f
memekode <rlopez@elmwoodbaptist.org>
2026-08-06 13:38:29 -0600

    Add Old West Sunday component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Leaders from "@/components/Leaders";
 import ServiceTimes from "@/components/ServiceTimes";
 import ChurchEvents from "@/components/ChurchEvents";
 import ManCamp from "@/components/ManCamp";
+import OldWestSunday from "@/components/OldWestSunday";
 import ScriptureBanner from "@/components/ScriptureBanner";
 import LatestSermon from "@/components/LatestSermon";
 import Give from "@/components/Give";
