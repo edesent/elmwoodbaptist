756968f12576496c38b8f167a8fcda9657a7690d
memekode <rlopez@elmwoodbaptist.org>
2026-09-03 12:33:37 -0600

    Add FallRevival import to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Leaders from "@/components/Leaders";
 import ServiceTimes from "@/components/ServiceTimes";
 import ChurchEvents from "@/components/ChurchEvents";
 import ManCamp from "@/components/ManCamp";
+import FallRevival from "@/components/FallRevival";
 import ScriptureBanner from "@/components/ScriptureBanner";
 import LatestSermon from "@/components/LatestSermon";
 import Give from "@/components/Give";
